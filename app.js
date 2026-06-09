55412e40dd87ef924da2dea8a7a0e372f69b1655
danizavtz <daniellucena@yahoo.com.br>
2026-06-09 12:12:09 -0300

    Add cors for frontend

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
 const express = require('express');
 const app = express();
-app.use(express.json());
+import cors from "cors";
+const allowedOrigins = [
+    "https://danizavtz.com.br",
+    "https://app.danizavtz.com.br"
+];
 
+app.use(express.json());
+app.use(cors({
+    origin: allowedOrigins,
+    methods: ["POST", "OPTIONS"],
+    allowedHeaders: ["Content-Type"],
+    credentials: false
+}));
 app.use(require('./server/index'));
 module.exports = app;
