7ba19eb3c4f3e94ea84acacf3c8c1008dfecc86b
danizavtz <daniellucena@yahoo.com.br>
2026-06-09 15:01:19 -0300

    Fix validator with bail

--- server/validators/content.validator.js
+++ server/validators/content.validator.js
@@ -1,12 +1,9 @@
 const { body, validationResult } = require('express-validator');
 
 exports.validationBodyRules = [
-    body('email', 'email é obrigatório').exists().bail(),
-    body('subject', 'assunto é obrigatório').exists().bail(),
-    body('message', 'mensagem é obrigatório').exists().bail(),
-    body('email').trim().notEmpty().withMessage('email não pode ser vazio').bail().isEmail().withMessage('email deve ser um email válido').bail(),
-    body('subject').trim().notEmpty().withMessage('assunto é obrigatório').bail().isLength({ max: 32 }).withMessage('assunto possui um tamanho máximo de 32 caracteres').bail(),
-    body('message').trim().notEmpty().withMessage('mensagem é obrigatório').bail().isLength({ max: 1000 }).withMessage('mensagem possui um tamanho máximo de 1000 caracteres').bail()
+    body('email').exists().withMessage('email é obrigatório').bail().trim().notEmpty().withMessage('email não pode ser vazio').bail().isEmail().withMessage('email deve ser um email válido').bail(),
+    body('subject').exists().withMessage('assunto é obrigatório').bail().trim().notEmpty().withMessage('assunto é obrigatório').bail().isLength({ max: 32 }).withMessage('assunto possui um tamanho máximo de 32 caracteres').bail(),
+    body('message').exists().withMessage('mensagem é obrigatório').bail().trim().notEmpty().withMessage('mensagem é obrigatório').bail().isLength({ max: 1000 }).withMessage('mensagem possui um tamanho máximo de 1000 caracteres').bail()
 ];
 
 exports.checkRules = (req, res, next) => {
