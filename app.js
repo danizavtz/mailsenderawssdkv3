89afcd25570eb4252cc933fbb72665962fa56021
danizavtz <daniellucena@yahoo.com.br>
2026-06-09 13:27:49 -0300

    Fix required in const declaration

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-import cors from "cors";
+const cors = require('cors');
 const allowedOrigins = [
     "https://danizavtz.com.br",
     "https://app.danizavtz.com.br"
